845cfc0c50cf608e068154687f2c9921d3b7e201
fer7G <fernand07gn@gmail.com>
2023-10-28 20:16:19 +0200

    UPDATE

--- Datasets/demand_per_hour.py
+++ Datasets/demand_per_hour.py
@@ -9,8 +9,8 @@ df_bikes = df_bikes.sort_values(by='last_reported')
 
 # Variables de control y acumulación
 hourly_data = []
-previous_mechanical = 0
-previous_ebike = 0
+disponibilidad_mecanicas_anterior = 0
+disponibilidad_electricas_anterior = 0
 previous_hour = df_bikes.iloc[0]['hour']
 salidas_mecanicas = 0
 salidas_electricas = 0
@@ -22,57 +22,68 @@ satisfy_demand = True
 # Constante para determinar cada cuántos instantes de tiempo consecutivos consideramos que no se ha satisfecho la demanda
 N = 1
 
-# Iterar sobre las filas del dataframe
+
+# --------------------------------------------------------------------------------------------------
+# BUCLE QUE ITERA LAS FILAS DEL DATAFRAME ##########################################################
+# --------------------------------------------------------------------------------------------------
+
+# -----> INICIALIZACION DE VARIABLES PREVIA AL BUCLE
 primera_salida_mechanical = False
 primera_salida_ebike = False
 balance_mechanical = 0
 balance_ebike = 0
-balance_minimo_mechanical = 100 # Número grande arbitrario
-balance_minimo_ebike = 100 # Número grande arbitrario
-new_hour = True
-first_mechanical = 0 # Valor inicial de bicicletas mecánicas a cada hora
-first_ebike = 0 # Valor inicial de bicicletas eléctricas a cada hora
+disponibilidad_minima_mecanicas_tras_primera_salida = 100 # Número grande arbitrario
+disponibilidad_minima_electricas_tras_primera_salida = 100 # Número grande arbitrario
+hora_nueva = True
+disponibilidad_mecanicas_inicial = 0 # Valor inicial de bicicletas mecánicas a cada hora
+disponibilidad_electricas_inicial = 0 # Valor inicial de bicicletas eléctricas a cada hora
+
+# -----> INICIO DEL BUCLE
 for index, row in df_bikes.iterrows():
-    current_mechanical = row['num_bikes_available_types.mechanical']
-    current_ebike = row['num_bikes_available_types.ebike']
+    disponibilidad_mecanicas_actual = row['num_bikes_available_types.mechanical']
+    disponibilidad_electricas_actual = row['num_bikes_available_types.ebike']
 
     # Guardar primeras cantidades si es la primera fila de la hora
-    if new_hour:
-        first_mechanical = current_mechanical
-        first_ebike = current_ebike
-        balance_mechanical = first_mechanical
-        balance_ebike = first_ebike
+    if hora_nueva:
+        disponibilidad_mecanicas_inicial = disponibilidad_mecanicas_actual
+        disponibilidad_electricas_inicial = disponibilidad_electricas_actual
+        balance_mechanical = disponibilidad_mecanicas_inicial
+        balance_ebike = disponibilidad_electricas_inicial
         diferencia_acumulada_mechanical = 0
         diferencia_acumulada_ebike = 0
-        new_hour = False
+        hora_nueva = False
     
-    # Acumular salidas y entradas
-    diff_mechanical = current_mechanical - previous_mechanical
-    diff_ebike = current_ebike - previous_ebike
-    diferencia_acumulada_mechanical += diff_mechanical
-    diferencia_acumulada_ebike += diff_ebike
-    balance_mechanical += diff_mechanical
-    balance_ebike += diff_ebike
+    # Calcular diferencias por cada cambio de fila
+    diferencia_mecanicas = disponibilidad_mecanicas_actual - disponibilidad_mecanicas_anterior
+    diferencia_electricas = disponibilidad_electricas_actual - disponibilidad_electricas_anterior
+    diferencia_acumulada_mechanical += diferencia_mecanicas
+    diferencia_acumulada_ebike += diferencia_electricas
+    # balance_mechanical += diferencia_mecanicas
+    # balance_ebike += diferencia_electricas
 
-    if diff_mechanical < 0:
+    # Si se han producido salidas de mecanicas...
+    if diferencia_mecanicas < 0:
         if not primera_salida_mechanical:
             primera_salida_mechanical = True
-        salidas_mecanicas += abs(diff_mechanical)
+        salidas_mecanicas += abs(diferencia_mecanicas)
         # Al haber habido salida, vemos si se ha alcanzado un mínimo
-        if (primera_salida_mechanical and balance_mechanical < balance_minimo_mechanical):
-            balance_minimo_mechanical = balance_mechanical
+        if (primera_salida_mechanical and disponibilidad_mecanicas_actual < disponibilidad_minima_mecanicas_tras_primera_salida):
+            disponibilidad_minima_mecanicas_tras_primera_salida = disponibilidad_mecanicas_actual
+    # Sino si se han producido entradas de mecanicas...
     else:
-        entradas_mecanicas += diff_mechanical
+        entradas_mecanicas += diferencia_mecanicas
 
-    if diff_ebike < 0:
+    # Si se han producido salidas de electricas...
+    if diferencia_electricas < 0:
         if not primera_salida_ebike:
             primera_salida_ebike = True
-        salidas_electricas += abs(diff_ebike)
+        salidas_electricas += abs(diferencia_electricas)
         # Al haber habido salida, vemos si se ha alcanzado un mínimo
-        if (primera_salida_ebike and balance_ebike < balance_minimo_ebike):
-            balance_minimo_ebike = balance_ebike
+        if (primera_salida_ebike and disponibilidad_electricas_actual < disponibilidad_minima_electricas_tras_primera_salida):
+            disponibilidad_minima_electricas_tras_primera_salida = disponibilidad_electricas_actual
+    # Sino si se han producido entradas de electricas...
     else:
-        entradas_electricas += diff_ebike
+        entradas_electricas += diferencia_electricas
     
     # Verificar si no hay bicicletas disponibles
     if row['num_bikes_available'] == 0:
@@ -88,40 +99,43 @@ for index, row in df_bikes.iterrows():
     if row['hour'] != previous_hour:
 
         if not primera_salida_mechanical:
-            balance_minimo_mechanical = 0
+            disponibilidad_minima_mecanicas_tras_primera_salida = 0
         if not primera_salida_ebike:
-            balance_minimo_ebike = 0
+            disponibilidad_minima_electricas_tras_primera_salida = 0
 
         total_salidas = salidas_mecanicas + salidas_electricas
         total_entradas = entradas_mecanicas + entradas_electricas
-        demanda_mecanicas = salidas_mecanicas - entradas_mecanicas
-        demanda_electricas = salidas_electricas - entradas_electricas
-        total_demanda = total_salidas - total_entradas
+
         if diferencia_acumulada_mechanical != 0:
-            min_required_mechanical = first_mechanical - balance_minimo_mechanical
+            min_required_mechanical = disponibilidad_mecanicas_inicial - disponibilidad_minima_mecanicas_tras_primera_salida
+            # Si es negativo, decimos que es 0, ya que en principio no consideraremos excedentes
+            if min_required_mechanical < 0:
+                min_required_mechanical = 0
         else:
             min_required_mechanical = 0
         if diferencia_acumulada_ebike != 0:
-            min_required_ebike = first_ebike - balance_minimo_ebike
+            min_required_ebike = disponibilidad_electricas_inicial - disponibilidad_minima_electricas_tras_primera_salida
+            # Si es negativo, decimos que es 0, ya que en principio no consideraremos excedentes
+            if min_required_ebike < 0:
+                min_required_ebike = 0
         else:
             min_required_ebike = 0
         
-        hourly_data.append([total_salidas, salidas_mecanicas, salidas_electricas, 
-                            entradas_mecanicas, entradas_electricas, total_entradas, 
-                            demanda_mecanicas, demanda_electricas, total_demanda, 
+        hourly_data.append([min_required_ebike, min_required_mechanical, total_salidas, salidas_electricas, salidas_mecanicas, 
+                            total_entradas, entradas_electricas, entradas_mecanicas,
                             previous_hour, row['day_week'], row['day_month'], 
-                            row['month'], satisfy_demand, min_required_mechanical, min_required_ebike])
+                            row['month'], satisfy_demand])
         
         # Reinicio de variables para la nueva hora
-        new_hour = True
-        first_mechanical = 0 # Valor inicial de bicicletas mecánicas a cada hora
-        first_ebike = 0 # Valor inicial de bicicletas eléctricas a cada hora
+        hora_nueva = True
+        disponibilidad_mecanicas_inicial = 0 # Valor inicial de bicicletas mecánicas a cada hora
+        disponibilidad_electricas_inicial = 0 # Valor inicial de bicicletas eléctricas a cada hora
         primera_salida_mechanical = False
         primera_salida_ebike = False
         balance_mechanical = 0
         balance_ebike = 0
-        balance_minimo_mechanical = 100 # Número grande arbitrario
-        balance_minimo_ebike = 100 # Número grande arbitrario
+        disponibilidad_minima_mecanicas_tras_primera_salida = 100 # Número grande arbitrario
+        disponibilidad_minima_electricas_tras_primera_salida = 100 # Número grande arbitrario
 
         salidas_mecanicas = 0
         salidas_electricas = 0
@@ -131,14 +145,13 @@ for index, row in df_bikes.iterrows():
         satisfy_demand = True
         previous_hour = row['hour']
     
-    previous_mechanical = current_mechanical
-    previous_ebike = current_ebike
+    disponibilidad_mecanicas_anterior = disponibilidad_mecanicas_actual
+    disponibilidad_electricas_anterior = disponibilidad_electricas_actual
 
 # Convertir la lista acumulada a un DataFrame
-columns = ['total_salidas', 'salidas_mecanicas', 'salidas_electricas', 
-           'entradas_mecanicas', 'entradas_electricas', 'total_entradas', 
-           'demanda_mecanicas', 'demanda_electricas', 'total_demanda', 
-           'hour', 'day_week', 'day_month', 'month', 'satisfy_demand', 'min_required_mechanical', 'min_required_ebike']
+columns = ['min_required_ebike', 'min_required_mechanical','total_salidas', 'salidas_electricas', 'salidas_mecanicas', 
+           'total_entradas', 'entradas_electricas', 'entradas_mecanicas',
+           'hour', 'day_week', 'day_month', 'month', 'satisfy_demand']
 df_hourly = pd.DataFrame(hourly_data, columns=columns)
 
 # Escribir el DataFrame resultante en un nuevo archivo llamado DataFramePerHour.csv
