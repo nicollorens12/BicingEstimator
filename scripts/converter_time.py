a28eac72ccb8e1f7d3294767bb948a4193525fd1
nicollorens12 <nickskate23@gmail.com>
2023-12-30 23:58:20 +0100

    Scripts Working

--- scripts/converter_time.py
+++ scripts/converter_time.py
@@ -1,21 +1,23 @@
 import pandas as pd
 
 # Cargar el conjunto de datos original
-data = pd.read_csv('barcelona_meteo_info.csv')
+data = pd.read_csv('scripts/TOTAL_meteo.csv')
 
-# Convertir la columna 'time' a formato datetime
-data['time'] = pd.to_datetime(data['time'])
+# Convertir la columna 'date' a formato datedate
+data['date'] = pd.to_datetime(data['date'])
 
 # Crear nuevas columnas 'year', 'month', 'day', 'hour'
-data['year'] = data['time'].dt.year
-data['month'] = data['time'].dt.month
-data['day'] = data['time'].dt.day
-data['hour'] = data['time'].dt.hour
+data['year'] = data['date'].dt.year
+data['month'] = data['date'].dt.month
+data['day'] = data['date'].dt.day
+data['hour'] = data['date'].dt.hour
 
-# Eliminar la columna 'time'
-data = data.drop(columns=['time'])
+# Eliminar la columna 'date'
+data = data.drop(columns=['date'])
+
+data = data[(data['year'] != 2020) & ((data['year'] < 2023) | ((data['year'] == 2023) & (data['month'] <= 11)))]
 
 # Guardar el nuevo conjunto de datos en un nuevo archivo CSV
-data.to_csv('barcelona_meteo_info.csv', index=False)
+data.to_csv('scripts/barcelona_meteo_info.csv', index=False)
 
 print("Proceso completado. El nuevo archivo CSV ha sido creado.")
